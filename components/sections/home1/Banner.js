639c2e52cb167b3ce6aa5b4502945221fe49b5e3
ismail-is <ismailthalithanooji@gmail.com>
2025-02-19 14:43:09 +0530

    day 2

--- components/sections/home1/Banner.js
+++ components/sections/home1/Banner.js
@@ -34,28 +34,25 @@ export default function Banner() {
               <SwiperSlide className="swiper-slide">
                 <div
                   className="main-slider__bg"
-                  style={{ backgroundImage: "url(./assets/images/allimgs/banner/Banner1.webp)" }}
+                  style={{ backgroundImage: "url(https://worldwide.sa/assets/img/slider/7.jpg)" }}
                 />
-                <div className="main-slider__shape-1" />
+                {/* <div className="main-slider__shape-1" />
                 <div className="main-slider__shape-2" />
                 <div className="main-slider__shape-3" />
-                <div className="main-slider__shape-4" />
+                <div className="main-slider__shape-4" /> */}
                 <div className="container">
                   <div className="row">
                     <div className="col-xl-12">
                       <div className="main-slider__content">
                         <div className="main-slider__sub-title-box">
                           <div className="main-slider__sub-title-icon">
-                            {/* <img
-                              src="assets/images/icon/main-slider-sub-title-icon.png"
-                              alt=""
-                            /> */}
+                           
                           </div>
                           {/* <p className="main-slider__sub-title">DELCO</p> */}
                         </div>
-                        <h2 className="main-slider__title">
+                        {/* <h2 className="main-slider__title">
                         Achieving highest <br /> standards <span> of quality </span> <br /> reliability, trust<br />an <span>satisfaction</span>
-                        </h2>
+                        </h2> */}
                         {/* <p className="main-slider__text">
                           We have been operating for over a decade, providing top-notch
                           services to <br /> our clients and building a strong track record in
@@ -68,24 +65,14 @@ export default function Banner() {
                               <span />
                             </Link>
                           </div>
-                          {/* <div className="main-slider__call">
-                            <div className="main-slider__call-icon">
-                              <span className="icon-phone" />
-                            </div>
-                            <div className="main-slider__call-number">
-                              <p>Need help?</p>
-                              <h5>
-                                <Link href="tel:307555-0133">(307) 555-0133</Link>
-                              </h5>
-                            </div>
-                          </div> */}
+                         
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </SwiperSlide>
-
+{/* second */}
               <SwiperSlide className="swiper-slide">
                 <div
                   className="main-slider__bg"
@@ -100,22 +87,12 @@ export default function Banner() {
                     <div className="col-xl-12">
                       <div className="main-slider__content">
                         <div className="main-slider__sub-title-box">
-                          {/* <div className="main-slider__sub-title-icon">
-                            <img
-                              src="assets/images/icon/main-slider-sub-title-icon.png"
-                              alt=""
-                            />
-                          </div> */}
-                          {/* <p className="main-slider__sub-title">Best shipping</p> */}
+                         
                         </div>
                         <h2 className="main-slider__title">
                         Our commitment <span>to excellence,</span> <br />  <span>reliability, </span> and customer <br/>satisfaction sets us apart <br/>in the <span>competitive market</span>  
                         </h2>
-                        {/* <p className="main-slider__text">
-                          We have been operating for over a decade, providing top-notch
-                          services to <br /> our clients and building a strong track record in
-                          the industry.
-                        </p> */}
+                     
                         <div className="main-slider__btn-and-call-box">
                           <div className="main-slider__btn-box">
                             <Link href="/about" className="thm-btn main-slider__btn">
@@ -123,133 +100,18 @@ export default function Banner() {
                               <span />
                             </Link>
                           </div>
-                          {/* <div className="main-slider__call">
-                            <div className="main-slider__call-icon">
-                              <span className="icon-phone" />
-                            </div>
-                            <div className="main-slider__call-number">
-                              <p>Need help?</p>
-                              <h5>
-                                <Link href="tel:307555-0133">(307) 555-0133</Link>
-                              </h5>
-                            </div>
-                          </div> */}
+                        
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </SwiperSlide>
+{/* second */}
 
-              {/* <SwiperSlide className="swiper-slide">
-                <div
-                  className="main-slider__bg"
-                  style={{ backgroundImage: "url(assets/images/backgrounds/slider-1-3.jpg)" }}
-                />
-                <div className="main-slider__shape-1" />
-                <div className="main-slider__shape-2" />
-                <div className="main-slider__shape-3" />
-                <div className="main-slider__shape-4" />
-                <div className="container">
-                  <div className="row">
-                    <div className="col-xl-12">
-                      <div className="main-slider__content">
-                        <div className="main-slider__sub-title-box">
-                          <div className="main-slider__sub-title-icon">
-                            <img
-                              src="assets/images/icon/main-slider-sub-title-icon.png"
-                              alt=""
-                            />
-                          </div>
-                          <p className="main-slider__sub-title">Best shipping</p>
-                        </div>
-                        <h2 className="main-slider__title">
-                          Reliable <span>Responsive</span> <br /> Driven Logistics
-                        </h2>
-                        <p className="main-slider__text">
-                          We have been operating for over a decade, providing top-notch
-                          services to <br /> our clients and building a strong track record in
-                          the industry.
-                        </p>
-                        <div className="main-slider__btn-and-call-box">
-                          <div className="main-slider__btn-box">
-                            <Link href="/about" className="thm-btn main-slider__btn">
-                              Read more
-                              <span />
-                            </Link>
-                          </div>
-                          <div className="main-slider__call">
-                            <div className="main-slider__call-icon">
-                              <span className="icon-phone" />
-                            </div>
-                            <div className="main-slider__call-number">
-                              <p>Need help?</p>
-                              <h5>
-                                <Link href="tel:307555-0133">(307) 555-0133</Link>
-                              </h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </SwiperSlide> */}
 
-              {/* <SwiperSlide className="swiper-slide">
-                <div
-                  className="main-slider__bg"
-                  style={{ backgroundImage: "url(assets/images/backgrounds/slider-1-1.jpg)" }}
-                />
-                <div className="main-slider__shape-1" />
-                <div className="main-slider__shape-2" />
-                <div className="main-slider__shape-3" />
-                <div className="main-slider__shape-4" />
-                <div className="container">
-                  <div className="row">
-                    <div className="col-xl-12">
-                      <div className="main-slider__content">
-                        <div className="main-slider__sub-title-box">
-                          <div className="main-slider__sub-title-icon">
-                            <img
-                              src="assets/images/icon/main-slider-sub-title-icon.png"
-                              alt=""
-                            />
-                          </div>
-                          <p className="main-slider__sub-title">Best shipping</p>
-                        </div>
-                        <h2 className="main-slider__title">
-                          Reliable <span>Responsive</span> <br /> Driven Logistics
-                        </h2>
-                        <p className="main-slider__text">
-                          We have been operating for over a decade, providing top-notch
-                          services to <br /> our clients and building a strong track record in
-                          the industry.
-                        </p>
-                        <div className="main-slider__btn-and-call-box">
-                          <div className="main-slider__btn-box">
-                            <Link href="/about" className="thm-btn main-slider__btn">
-                              Read more
-                              <span />
-                            </Link>
-                          </div>
-                          <div className="main-slider__call">
-                            <div className="main-slider__call-icon">
-                              <span className="icon-phone" />
-                            </div>
-                            <div className="main-slider__call-number">
-                              <p>Need help?</p>
-                              <h5>
-                                <Link href="tel:307555-0133">(307) 555-0133</Link>
-                              </h5>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </SwiperSlide> */}
+            
+            
             </div>
           </Swiper>
           <div className="swiper-pagination" id="main-slider-pagination" />
