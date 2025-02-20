f9785e85e4107209ad17183ed1228d3e13cf9c79
ismail-is <ismailthalithanooji@gmail.com>
2025-02-20 11:45:09 +0530

    20-2-2025 1

--- components/sections/home1/Banner.js
+++ components/sections/home1/Banner.js
@@ -34,7 +34,7 @@ export default function Banner() {
               <SwiperSlide className="swiper-slide">
                 <div
                   className="main-slider__bg"
-                  style={{ backgroundImage: "url(https://worldwide.sa/assets/img/slider/7.jpg)" }}
+                  style={{ backgroundImage: "url(./assets/images/allimgs/banner/banner1.webp)" }}
                 />
                 {/* <div className="main-slider__shape-1" />
                 <div className="main-slider__shape-2" />
@@ -59,12 +59,55 @@ export default function Banner() {
                           the industry.
                         </p> */}
                         <div className="main-slider__btn-and-call-box">
-                          <div className="main-slider__btn-box">
+                          {/* <div className="main-slider__btn-box">
                             <Link href="/services" className="thm-btn main-slider__btn">
                              Our Services
                               <span />
                             </Link>
+                          </div> */}
+                         
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+
+
+               <SwiperSlide className="swiper-slide">
+                <div
+                  className="main-slider__bg"
+                  style={{ backgroundImage: "url(./assets/images/allimgs/banner/banner1.webp)" }}
+                />
+                {/* <div className="main-slider__shape-1" />
+                <div className="main-slider__shape-2" />
+                <div className="main-slider__shape-3" />
+                <div className="main-slider__shape-4" /> */}
+                <div className="container">
+                  <div className="row">
+                    <div className="col-xl-12">
+                      <div className="main-slider__content">
+                        <div className="main-slider__sub-title-box">
+                          <div className="main-slider__sub-title-icon">
+                           
                           </div>
+                          {/* <p className="main-slider__sub-title">DELCO</p> */}
+                        </div>
+                        {/* <h2 className="main-slider__title">
+                        Achieving highest <br /> standards <span> of quality </span> <br /> reliability, trust<br />an <span>satisfaction</span>
+                        </h2> */}
+                        {/* <p className="main-slider__text">
+                          We have been operating for over a decade, providing top-notch
+                          services to <br /> our clients and building a strong track record in
+                          the industry.
+                        </p> */}
+                        <div className="main-slider__btn-and-call-box">
+                          {/* <div className="main-slider__btn-box">
+                            <Link href="/services" className="thm-btn main-slider__btn">
+                             Our Services
+                              <span />
+                            </Link>
+                          </div> */}
                          
                         </div>
                       </div>
@@ -73,7 +116,7 @@ export default function Banner() {
                 </div>
               </SwiperSlide>
 {/* second */}
-              <SwiperSlide className="swiper-slide">
+              {/* <SwiperSlide className="swiper-slide">
                 <div
                   className="main-slider__bg"
                   style={{ backgroundImage: "url(./assets/images/allimgs/banner/Banner2.webp)" }}
@@ -106,7 +149,7 @@ export default function Banner() {
                     </div>
                   </div>
                 </div>
-              </SwiperSlide>
+              </SwiperSlide> */}
 {/* second */}
 
 
